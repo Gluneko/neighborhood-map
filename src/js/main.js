131dd9e86fa4eeb3b147aa6dc90fc58cdbeeca04
Gluneko <844410616@qq.com>
2016-12-20 20:19:21 +0800

    Can't display the list

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,28 @@
       var markers = [];
       // Create placemarkers array to use in multiple functions to have control
       // over the number of places that show.
-      var placeMarkers = [];
+
      //Initialize the map
      function initMap() {
 
-      var sv={lat:37.387474,lng:-122.057543};
-       //  Use a constructor to create a new map JS object.
+      var sv={lat:37.387474,lng:-122.057543},
+       //  Create a new map center in Silicon Valley.
        map=new google.maps.Map(document.getElementById('map'),{
           center:sv,
           zoom:13
        });
+
+       var request = {
+        location: sv,
+        radius: '5000',
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, nearbyCallback);
+
+
+
+
        // Create a searchbox in order to execute a places search
         var searchBox = new google.maps.places.SearchBox(
             document.getElementById('places-search'));
@@ -23,16 +35,63 @@
        var largeInfowindow = new google.maps.InfoWindow();
 
        // Style the markers a bit. This will be our listing marker icon.
-        var defaultIcon = makeMarkerIcon('0091ff');
+        //var defaultIcon = makeMarkerIcon('0091ff');
 
          // Create a "highlighted location" marker color for when the user
         // mouses over the marker.
-        var highlightedIcon = makeMarkerIcon('FFFF24');
-     }
+        //var highlightedIcon = makeMarkerIcon('FFFF24');
 
-// var ViewModel=function () {
-//   var self=this;
+         function createMarker(place) {
+        //var placeLoc = place.geometry.location;
+        var icon = {
+            url: place.icon,
+            size: new google.maps.Size(35, 35),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(15, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          //label: labels[labelIndex++ % labels.length],
+          title: place.name,
+          icon: icon,
+          id: place.place_id,
+          animation: google.maps.Animation.DROP
+        });
+         // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open the large infowindow at each marker
+        google.maps.event.addListener(marker, 'click', function() {
+          largeInfowindow.setContent(place.name);
+          largeInfowindow.open(map, this);
+        });
+      }
 
-// }
+      function nearbyCallback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    }
 
-// ko.applyBindings(new ViewModel());
+    var ViewModel=function () {
+      var self=this;
+      self.list=ko.observableArray([]);
+      // Here's the problem:the markers array is empty when executing this function,
+      //after a few seconds it's not empty,however the list shows nothing。
+      markers.forEach( function(marker) {
+            self.list.push(marker.title);
+            console.log(marker.title);
+        });
+      };
+      vm = new ViewModel();
+      ko.applyBindings(vm);
+}
+
+function googleError(){
+  alert('failed!');
+}
+
+
