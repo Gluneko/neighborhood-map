235141878525fb370ba45c3251ac81e1cc183a1f
Gluneko <844410616@qq.com>
2016-12-25 00:25:57 +0800

    Add filter function

--- src/js/main.js
+++ src/js/main.js
@@ -12,18 +12,72 @@
        //  Create a new map center in Silicon Valley.
        map=new google.maps.Map(document.getElementById('map'),{
           center:sv,
-          zoom:13
+          zoom:13,
+          mapTypeControl: false
        });
 
-       var request = {
-        location: sv,
-        radius: '5000',
-      };
+      //  var request = {
+      //   location: sv,
+      //   radius: '5000',
+      // };
 
+      // This autocomplete is for use in the geocoder entry box.
+       var placesSearch = new google.maps.places.Autocomplete(
+        (document.getElementById('places-search')),
+        {types: ['geocode']});
+        // var placesSearch = new google.maps.places.Autocomplete(
+        //     document.getElementById('places-search'));
+
+        // Listen for the event fired when the user selects a prediction from the
+        // picklist and retrieve more details for that place.
+        placesSearch.addListener('place_changed', searchPlaces);
+
+
+      // This function takes the input value in the find area text input
+      // locates it, and then zooms into that area. This is so that the user can
+      // show all listings, then decide to focus on one area of the map.
+      function searchPlaces() {
+        // Initialize the geocoder.
+        //  console.log(place);
+        // var geocoder = new google.maps.Geocoder();
+        // // Get the address or place that the user entered.
+        // var address = document.getElementById('places-search').value;
+        // // Make sure the address isn't blank.
+        // if (address == '') {
+        //   window.alert('You must enter an area, or address.');
+        // } else {
+        //   // Geocode the address/area entered to get the center. Then, center the map
+        //   // on it and zoom in
+        //   geocoder.geocode(
+        //     { address: address,
+        //       //componentRestrictions: {locality: 'New York'}
+        //     }, function(results, status) {
+        //       if (status == google.maps.GeocoderStatus.OK) {
+        //         //console.log(results[0]);
+        //         map.setCenter(results[0].geometry.location);
+        //         map.setZoom(13);
+        //       } else {
+        //         window.alert('We could not find that location - try entering a more' +
+        //             ' specific place.');
+        //       }
+        //     });
+        // }
+
+        // Get the place details from the autocomplete object.
+        var place = placesSearch.getPlace(),
+        center=place.address_components[0].short_name;
+        if(typeof(center)== "undefined"){
+          return;
+        }
+        vm.center(center);
+        map.setCenter(place.geometry.location);
+        newPlaceSearch(place.formatted_address);
+      }
      // service = new google.maps.places.PlacesService(map);
      // service.nearbySearch(request, nearbyCallback);
 
       function nearbySearch(results) {
+        hideMarkers(vm.list());
         vm.list.removeAll();
         var all=results.businesses;
         var i=0;
@@ -33,6 +87,7 @@
         }
           createMarker(place,++i);
         });
+        showListings();
         // for (var i = 0,len=all.length; i < len; i++) {
         //   if(typeof(all[i].location.coordinate)== "undefined")
         //     continue;
@@ -43,6 +98,29 @@
           //console.log(all[i].location.coordinate);
         //}
     }
+
+      // This function will loop through the markers array and display them all.
+      function showListings() {
+        var bounds = new google.maps.LatLngBounds(),
+        // Extend the boundaries of the map for each marker and display the marker
+        markers=vm.listFiltered(),
+        len=markers.length;
+        if(len===0) {
+          return;
+        }
+        for (var i = 0; i < len; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+      // This function will loop through the listings and hide them all.
+      function hideMarkers(markers) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      }
+
         /**
  * Generates a random number and returns it as a string for OAuthentication
  * @return {string}
@@ -57,7 +135,10 @@ var yelp_url = 'http://api.yelp.com/v2/search',
     yelp_token='KhvBiuMmDYHKB9L9B5wzoNr9XywM7Dxh',
     yelp_token_secret='wDzMn0gVn8xKBu4l26soAIg1f9E';
 
-    var parameters = {
+newPlaceSearch('Silicon+Valley');
+
+function newPlaceSearch (location) {
+      var parameters = {
       oauth_consumer_key: yelp_key,
       oauth_token: yelp_token,
       oauth_nonce: nonce_generate(),
@@ -65,7 +146,7 @@ var yelp_url = 'http://api.yelp.com/v2/search',
       oauth_signature_method: 'HMAC-SHA1',
       oauth_version : '1.0',
       callback: 'cb',              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-      location:'Silicon+Valley',
+      location:location,
       // cll:sv.lat+','+sv.lng,
     };
 
@@ -91,14 +172,13 @@ var yelp_url = 'http://api.yelp.com/v2/search',
 
     // Send AJAX query via jQuery library.
     $.ajax(settings);
-
-
+}
 
        // Create a searchbox in order to execute a places search
-        var searchBox = new google.maps.places.SearchBox(
-            document.getElementById('places-search'));
-        // Bias the searchbox to within the bounds of the map.
-        searchBox.setBounds(map.getBounds());
+        // var searchBox = new google.maps.places.SearchBox(
+        //     document.getElementById('places-search'));
+        // // Bias the searchbox to within the bounds of the map.
+        // searchBox.setBounds(map.getBounds());
 
        var largeInfowindow = new google.maps.InfoWindow();
 
@@ -122,14 +202,14 @@ var yelp_url = 'http://api.yelp.com/v2/search',
         latlng.lat=coordinate.latitude;
         latlng.lng=coordinate.longitude;
         var marker = new google.maps.Marker({
-          map: map,
+          // map: map,
           position: latlng,
           //label: i.toString(),
           title: place.name,
           //console.log(place.name);
           //icon: icon,
-          //id: place.id,
-          id: i,
+          id: place.id,
+          //id: i,
           animation: google.maps.Animation.DROP
         });
         marker.place=place;
@@ -174,6 +254,7 @@ var yelp_url = 'http://api.yelp.com/v2/search',
           //rating_img_small=place.rating_img_url_small,
           //rating_img_large=place.rating_img_url_large,
           review_count=place.review_count;
+          //console.log(address);
           var content='<div class="info"><section class="info-left">'+'<img src="'+img+'">'+'</section>'+'<section class="info-right">'+
             '<div class="info-name"><a class="info-link" href="'+url+'">'+name+'</a></div>'+'<div class="info-addr">'+address+'</div>'+
             '<div class="info-rating">'+
@@ -193,13 +274,28 @@ var yelp_url = 'http://api.yelp.com/v2/search',
     var ViewModel=function () {
       var self=this;
       self.list=ko.observableArray([]);
+      self.center=ko.observable('Silicon Valley');
+      self.querystr=ko.observable('');
+      self.listFiltered=ko.computed(function () {
+        if($.trim(self.querystr()).length==0){
+          return self.list();
+        }else {
+          return ko.utils.arrayFilter(self.list(), function(marker) {
+            return marker.title.toLowerCase().indexOf(self.querystr().toLowerCase())>-1;
+        });
+        }
+      });
+      self.filter=function () {
+        hideMarkers(self.list());
+        showListings();
+      };
      // self.navigateEnabled=ko.observable(false);
       self.listMouseOver=function (marker) {
         marker.setAnimation(google.maps.Animation.BOUNCE);
         //self.navigateEnabled(true);
-        console.log(marker.navigateEnabled);
+        //console.log(marker.navigateEnabled);
         marker.navigateEnabled=true;
-        console.log(marker.navigateEnabled);
+        //console.log(marker.navigateEnabled);
       };
       self.listMouseOut=function (marker) {
         marker.setAnimation(null);
@@ -208,6 +304,7 @@ var yelp_url = 'http://api.yelp.com/v2/search',
         //console.log(marker.navigateEnabled);
       };
       self.listClick=function (marker) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
         populateInfoWindow(marker, largeInfowindow);
       };
 
